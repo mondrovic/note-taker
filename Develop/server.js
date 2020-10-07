be9ba0dff659f70608a125372faca44864282459
Matthew O <mo@pop-os.localdomain>
2020-10-06 20:03:17 -0700

    began setting up server

---
+++ Develop/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const { db } = require("./db/db.json");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// parse incoming string or array
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+// get for db.json
+app.get("/api/notes", (req, res) => {
+  let results = db;
+
+  res.json(results);
+});
+
+// get for index.html
+// app.get("/notes", (req, res) => {
+//   console.log(req, res);
+// });

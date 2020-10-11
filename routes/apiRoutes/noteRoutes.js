439d437641c9cf44eabcfa289e76f70080fbe910
Matthew O <mo@pop-os.localdomain>
2020-10-11 14:01:43 -0700

    POST/GET functions finished

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -6,10 +6,7 @@ const { notes } = require("../../db/db.json");
 
 // get for db.json
 router.get("/notes", (req, res) => {
-  let results = db;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
+  let results = notes;
   res.json(results);
 });
 
@@ -25,7 +22,7 @@ router.get("/notes/:id", (req, res) => {
 
 // posts changes to db and checks if response is valid
 router.post("/notes", (req, res) => {
-  // parses request id to string
+  // assigns req.body.id to length of database
   req.body.id = notes.length.toString();
 
   // checks if formatted properly
@@ -33,7 +30,7 @@ router.post("/notes", (req, res) => {
     res.status(400).send("The note is not properly formatted");
   } else {
     // creates new note with request body
-    const note = createNote(req.body, db);
+    const note = createNote(req.body, notes);
     res.json(note);
   }
 });

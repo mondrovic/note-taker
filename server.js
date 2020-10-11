bd736586bf9d7f3835f508ebb6676570018ef646
Matthew O <mo@pop-os.localdomain>
2020-10-11 10:59:13 -0700

    cleaned directory

--- server.js
+++ server.js
@@ -1,84 +1,30 @@
 const express = require("express");
-const { db } = require("./db/db.json");
-const fs = require("fs");
-const path = require("path");
+// index is default file read if one is available
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 // parse incoming string or array
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+//adds custom routes instead of hard coding to app.get (/api is for api commands and / is for html pages)
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+// accepts public file for files like CSS or public side JS
+app.use(express.static("public"));
 
-// finds by id
-function findById(id, noteArray) {
-  const result = noteArray.filter((note) => note.id === id)[0];
-  return result;
-}
-
-// get for db.json
-app.get("/api/notes", (req, res) => {
-  let results = db;
-  res.send("This is");
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
 
-//get for specific id
-app.get("/api/notes/:id", (req, res) => {
-  const result = findById(req.params.id, notes);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 //html pages
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
-});
-
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-
-// posts changes to db and checks if response is valid
-app.post("/api/notes", (req, res) => {
-  // parses request id to string
-  req.body.id = notes.length.toString();
-
-  // checks if formatted properly
-  if (!validateNote(req.body)) {
-    res.status(400).send("The note is not properly formatted");
-  } else {
-    // creates new note with request body
-    const note = createNote(req.body, db);
-    res.json(note);
-  }
-});
-
-// creates a note based on passed in req.body & note db
-function createNote(body, noteArray) {
-  const note = body;
-  noteArray.push(note);
-  fs.writeFileSync(
-    path.join(__dirname, "./db/db.json"),
-    JSON.stringify({ notes: noteArray }, null, 2)
-  );
-  return note;
-}
-
-// checks if title and text are strings
-function validateNote(note) {
-  if (!note.title || typeof note.title !== "string") {
-    return false;
-  }
-  if (!note.text || typeof note.text !== "string") {
-    return false;
-  }
-}
-
-// adds port to listen on
-app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
 });
